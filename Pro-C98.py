8d03745b04fee5853ee92f656ca81733dd2d3e51
SHAHAZADISHAGUFTHA <64363861+SHAHAZADISHAGUFTHA@users.noreply.github.com>
2021-02-26 20:57:39 +0530

    Update Pro-C98.py

--- Pro-C98.py
+++ Pro-C98.py
@@ -1,15 +1,15 @@
 def swapFilesData():
-    filename = input("Enter Your 1st File Name:-")
-    filename = input("Enter Your 2nd File Name:-")
-    with open("sample2.txt",'r') as a :
+    file1 = input("Enter Your 1st File Name:-")
+    file2 = input("Enter Your 2nd File Name:-")
+    with open(file1,'r') as a :
         data_a = a.read()
         
-    with open("sample1.txt",'r') as b :
+    with open(file2,'r') as b :
         data_b = b.read()
        
-    with open("sample2.txt",'w') as c :
+    with open(file1,'w') as c :
         c.write(data_b)
-    with open("sample1.txt",'w') as d :
+    with open(file2,'w') as d :
         d.write(data_a)
 
 swapFilesData()
